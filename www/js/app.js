7569882a72c1c42b7e617f6c9f978483d140f529
unknown <tiongyingkiat@gmail.com>
2015-04-18 22:22:31 +0800

    Version 1.1-20150418

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,15 @@
 // Ionic Starter App
 
+// Firebase initialization
+var  fb = new Firebase("https://luminous-heat-4733.firebaseio.com/");
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','firebase'])
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,6 +22,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+	
+
   });
 })
 
@@ -31,89 +37,92 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-	
-	
-	
+
   // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-  
-   .state('tab.selectRestaurant', {
-    url: '/selectRestaurant',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-selectRestaurant.html',
-        controller: 'SelectRestaurantCtrl'
-      }
-    }
-  })
-
-  .state('tab.favourite', {
-      url: '/favourite',
-      views: {
-        'tab-favourite': {
-          templateUrl: 'templates/tab-favourite.html',
-          controller: 'FavouriteCtrl'
-        }
-      }
-    })
-  
-	.state('tab.settings', {
-		url: '/settings',
-			views: {
-				'tab-settings': {
-					templateUrl: 'templates/tab-settings.html',
-					controller: 'SettingsCtrl'
-				}
-			}
-	})
-
-	.state('tab.myshelf', {
-			url: '/myshelf',
-			views: {
-				'tab-myshelf': {
-					templateUrl: 'templates/tab-myshelf.html',
-					controller: 'MyShelfCtrl'
-				}
-			}
-	})
-
-	.state('tab.myrestaurant', {
-			url: '/myrestaurant',
-			views: {
-				'tab-myshelf': {
-					templateUrl: 'templates/tab-myrestaurant.html',
-					controller: 'MyRestaurantCtrl'
-				}
-			}
+	.state('tab', {
+		url: "/tab",
+		abstract: true,
+		templateUrl: "templates/tabs.html"
 	})
 	
-	.state('tab.newrestaurant', {
-			url: '/newrestaurant',
-			views: {
-				'tab-myshelf': {
-					templateUrl: 'templates/tab-newrestaurant.html',
-					controller: 'NewRestaurantCtrl'
-				}
-			}
+	.state('login', {
+			url: '/login',
+			templateUrl: 'templates/login.html',
+			controller: 'LoginCtrl'
 	})
+	
+	  // Each tab has its own nav history stack:
+	.state('tab.dash', {
+		url: '/dash',
+		views: {
+		  'tab-dash': {
+			templateUrl: 'templates/tab-dash.html',
+			controller: 'DashCtrl'
+		  }
+		}
+	})
+		
+	   .state('tab.selectRestaurant', {
+		url: '/selectRestaurant',
+		views: {
+		  'tab-dash': {
+			templateUrl: 'templates/tab-selectRestaurant.html',
+			controller: 'SelectRestaurantCtrl'
+		  }
+		}
+	  })
+
+	  .state('tab.favourite', {
+		  url: '/favourite',
+		  views: {
+			'tab-favourite': {
+			  templateUrl: 'templates/tab-favourite.html',
+			  controller: 'FavouriteCtrl'
+			}
+		  }
+		})
+	  
+		.state('tab.settings', {
+			url: '/settings',
+				views: {
+					'tab-settings': {
+						templateUrl: 'templates/tab-settings.html',
+						controller: 'SettingsCtrl'
+					}
+				}
+		})
+
+		.state('tab.myshelf', {
+				url: '/myshelf',
+				views: {
+					'tab-myshelf': {
+						templateUrl: 'templates/tab-myshelf.html',
+						controller: 'MyShelfCtrl'
+					}
+				}
+		})
+
+		.state('tab.myrestaurant', {
+				url: '/myrestaurant',
+				views: {
+					'tab-myshelf': {
+						templateUrl: 'templates/tab-myrestaurant.html',
+						controller: 'MyRestaurantCtrl'
+					}
+				}
+		})
+		
+		.state('tab.newrestaurant', {
+				url: '/newrestaurant',
+				views: {
+					'tab-myshelf': {
+						templateUrl: 'templates/tab-newrestaurant.html',
+						controller: 'NewRestaurantCtrl'
+					}
+				}
+		})
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,78 @@ angular.module('starter.controllers', [])
 
 .controller('SelectRestaurantCtrl', function($scope) {})
 
+.controller('LoginCtrl', function($scope, $firebaseAuth, $location) {
+  $scope.login = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$authWithPassword({
+            email: username,
+            password: password
+        }).then(function(authData) {
+            alert("SUCCESS");
+			//$location.path("/tab/dash");
+        }).catch(function(error) {
+			alert("ERROR: " + error);
+            console.error("ERROR: " + error);
+        });
+    }
+ 
+    $scope.register = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$createUser({email: username, password: password}).then(function() {
+            return fbAuth.$authWithPassword({
+                email: username,
+                password: password
+            });
+        }).then(function(authData) {
+			//Store user (paid_account & created_dt) in db after registration
+			fb.child("users").child(fb.getAuth().uid).set({
+					created_dt: Firebase.ServerValue.TIMESTAMP
+			});
+            $location.path("/tab/dash");
+        }).catch(function(error) {
+			alert("ERROR: " + error);
+            console.error("ERROR " + error);
+        });
+    }
+})
+
 .controller('NewRestaurantCtrl', function($scope, $state) {
-	$scope.newRestaurant = function(){
+	$scope.newRestaurant = function(restaurant){
+		//Array to store user created restaurant
+		$scope.restaurant = [];
+		
+		//Creating restaurant at fb > restaurant
+		var newRestaurantRef= fb.child("restaurant");
+		var newRestaurantReturnRef = newRestaurantRef.push({
+			title: restaurant.title,
+			description: restaurant.desc,
+			promotion: restaurant.promo,
+			contact: restaurant.contact,
+			address: restaurant.add
+		});
+		
+		//Get the unique key created by push method
+		var newRestaurantKey = newRestaurantReturnRef.key();
+		
+		//To save the created restaurant at users child location
+		var userRef = fb.child("users").child(fb.getAuth().uid);	
+		
+		//Retrieve the existing restaurant created by the user
+		userRef.on("value", function(snapshot) {
+			var userData= snapshot.val();
+			$scope.restaurant = userData.my_restaurant;
+		});
+		
+		//Push the newly created restaurant (unique key) to the array
+		$scope.restaurant.push({
+			restaurant_key: newRestaurantKey
+		});
+		
+		//Save it to fb
+		userRef.update({	
+			my_restaurant: $scope.restaurant
+		});
+		
 		$state.go("tab.myshelf");	
 	};	
 })
@@ -35,12 +105,44 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MyShelfCtrl', function($scope, $ionicModal, $timeout, $state) {
+	//Populate list of user created restaurant(s)
+	// Get a reference to our posts
+	var userRef = fb.child("users").child(fb.getAuth().uid);
+	var restaurantRef = fb.child("restaurant");
+	
+	$scope.restaurant = [];
+	$scope.myListOfRestaurants = [];
+	
+	// Attach an asynchronous callback to read the data at our posts reference
+	userRef.on("value", function(snapshot) {
+		var userData= snapshot.val();
+		$scope.restaurant = userData.my_restaurant;
+		console.log(snapshot.val());
+		// Get the user's created restaurant from the Restaurant child in FireBase
+		for(var i=0; i < $scope.restaurant.length; i++) {
+			var myRestaurantRef = restaurantRef.child($scope.restaurant[i].restaurant_key);
+			
+			myRestaurantRef.on("value", function(snapshot) {
+				var myRestaurant = snapshot.val();
+				console.log(snapshot.val());
+				$scope.myListOfRestaurants.push(myRestaurant);
+				
+			}, function (errorObject) {
+			  console.log("The read failed: " + errorObject.code);
+			});
+		}		
+		
+	}, function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});
+	
+
 	//Create and Load the Modal
 	$ionicModal.fromTemplateUrl('myshelf.html', {
-    scope: $scope
-    }).then(function (modal) {
-        $scope.myShelfModal = modal;
-    });
+		scope: $scope
+		}).then(function (modal) {
+			$scope.myShelfModal = modal;
+	});
 	
 	//Open our new task modal
 	$scope.showMyShelfModal = function(){
@@ -51,9 +153,12 @@ angular.module('starter.controllers', [])
 	$scope.closeMyShelfModal = function(){
 		$scope.myShelfModal.hide();	
 	};
-
+	
 	$scope.$on('$ionicView.enter', function () {
-		$scope.showMyShelfModal();
+		var paid_account = 'N';
+		paid_account = window.localStorage['paid_account'];
+		if (paid_account != 'Y')
+				$scope.showMyShelfModal();
 	});
 	
 	//Create and Load the Modal (for adding a new shelf)
@@ -73,11 +178,15 @@ angular.module('starter.controllers', [])
 		$scope.newShelfModal.hide();	
 	};
 	
-	
 	$scope.newRestaurant = function(){
+		window.localStorage['paid_account'] = 'Y';
+		$scope.closeMyShelfModal();
 		$scope.closeNewShelfModal();
 		$state.go("tab.newrestaurant");	
 	};
+	
+
+		
 })
 
 
